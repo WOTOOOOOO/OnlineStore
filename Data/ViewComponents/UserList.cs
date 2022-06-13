c851f5efe1bb7e563e8026947badff66129916d1
WOTOOOOOO <giorgichavchavadze132@gmail.com>
2022-06-14 01:17:02 +0400

    added admin related view component

--- Data/ViewComponents/UserList.cs
+++ Data/ViewComponents/UserList.cs
@@ -1,6 +1,13 @@
-﻿namespace someOnlineStore.Data.ViewComponents
+﻿using Microsoft.AspNetCore.Mvc;
+using someOnlineStore.Models;
+
+namespace someOnlineStore.Data.ViewComponents
 {
-    public class UserList
+    public class UserList : ViewComponent
     {
+        public IViewComponentResult Invoke(IEnumerable<User> userList)
+        {
+            return View(userList);
+        }
     }
 }
