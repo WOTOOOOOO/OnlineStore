0bec14cbc306c891f6a05ea437591ae109e752d8
WOTOOOOOO <giorgichavchavadze132@gmail.com>
2022-06-14 01:16:04 +0400

    added profile edit view component front-end

---
+++ Data/ViewComponents/ProfileEdits.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using someOnlineStore.Data.ViewModels;
+
+namespace someOnlineStore.Data.ViewComponents
+{
+    public class ProfileEdits : ViewComponent
+    {
+        public IViewComponentResult Invoke(ProfileGeneralEditsVM userVM)
+        {
+            return View(userVM);
+        }
+    }
+}
